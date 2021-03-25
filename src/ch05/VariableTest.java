ae061b33ce2f3a3be2fd92ff60dc27971872b8ef
hwajeong <leehung55@gmail.com>
2021-03-25 18:15:12 +0900

    java

---
+++ src/ch05/VariableTest.java
@@ -0,0 +1,14 @@
+
+// 변수 선언과 사용하기 : 자료형과 변수 명으로 변수를 선언함. 선언과 동시에 초기화 할수 있음
+package ch05;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        int age, count ;
+        age = 10;
+
+        int level = 10000;
+        System.out.println(age);
+        System.out.println(level);
+    }
+}
