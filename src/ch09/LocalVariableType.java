c8d6a83eef33a22eeb3cb12fc705f465105357c8
leehjung55 <leehjung55@gmail.com>
2021-03-26 14:17:28 +0900

    java

---
+++ src/ch09/LocalVariableType.java
@@ -0,0 +1,18 @@
+//// 자바 10 버전 이상만 가능하다고 하는데,, 그래서 안되나부당...
+//package ch09;
+//
+//public class LocalVariableType {
+//    public static void main(String[] args) {
+//
+//        var i = 10;
+//        var j = 10.0;
+//        var str = "hello";
+//
+//        System.out.println(i);
+//        System.out.println(j);
+//        System.out.println(str);
+//
+////        str = "test";
+//        //str = '3';
+//    }
+//}
