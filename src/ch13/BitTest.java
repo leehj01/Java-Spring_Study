ab802edaed9841d979c93988d148a9878414c240
leehjung55 <leehjung55@gmail.com>
2021-03-26 16:49:58 +0900

    java_ch12, 13

---
+++ src/ch13/BitTest.java
@@ -0,0 +1,19 @@
+package ch13;
+
+public class BitTest {
+    public static void main(String[] args) {
+
+        int num1 = 5;  // 00000101
+        int num2 = 10; // 00001010
+
+        System.out.println(num1 | num2); // 15
+        System.out.println(num1 & num2 ); // 0
+        System.out.println(num1 ^ num2); //15
+        System.out.println(~num1); // -6
+
+        System.out.println(num1 << 2); // 20
+        System.out.println(num1); // 5
+        System.out.println(num1 <<= 2); // 20
+        System.out.println(num1); // 20
+    }
+}
