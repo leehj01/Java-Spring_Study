9def0043640f6f4f47e5f418049b7f54a2c7c794
leehjung55 <leehjung55@gmail.com>
2021-03-28 16:10:30 +0900

    README

---
+++ Chapter2/src/ch02/Student.java
@@ -0,0 +1,14 @@
+package ch02;
+
+// public 은 어디서든 사용가능하다 ! 라는 개념
+// 클래스가 여러개일 수 있지만, public 은 하나여야한다.
+public class Student {
+
+    int studentNumber ;
+    String studentName;
+    // String - 기본 자료형이 아니라, 자바에서 제공하는 클래스
+    // 문자열 길게 사용할 때 사용하는 클래스
+    int majorCode;
+    String majorName ; // 이런것을 멤버 변수라고함
+    int grade;
+}
