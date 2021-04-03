7b6215fe8e3e9cbbd22c46e674b51b93d3ee9f3b
leehjung55 <leehjung55@gmail.com>
2021-04-03 13:54:03 +0900

    chapter3 - ch5

---
+++ Chapter3/src/ch05/TestMethod.java
@@ -0,0 +1,17 @@
+package ch05;
+
+public class TestMethod {
+    int num;
+
+    void aaa(){
+        System.out.println("TestMethod.aaa 호출");
+    }
+
+    public static void main(String[] args) {
+        TestMethod a1 = new TestMethod();
+        a1.aaa();
+
+        TestMethod a2 = new TestMethod();
+        a2.aaa();
+    }
+}
