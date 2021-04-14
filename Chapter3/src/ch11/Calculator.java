64433bacadcf57653f931efbceff8c204e95e296
leehjung55 <leehjung55@gmail.com>
2021-04-14 09:17:47 +0900

    chapter3 - ch11

---
+++ Chapter3/src/ch11/Calculator.java
@@ -0,0 +1,15 @@
+package ch11;
+
+// 추상메서드를 다 포함하는게 아니기 때문에, 추상 클래스가 된다.
+public abstract class Calculator implements Calc{
+
+    @Override
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int substract(int num1, int num2) {
+        return num1 - num2;
+    }
+}
