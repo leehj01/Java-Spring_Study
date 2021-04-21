e39de7ab202c55943c57af0f4ec00b4782be1635
leehjung55 <leehjung55@gmail.com>
2021-04-21 09:56:10 +0900

    chapter3 - ch13

---
+++ Chapter3/src/ch13/domain/userinfo/dao/oracle/UserInfoOracleDao.java
@@ -0,0 +1,21 @@
+package ch13.domain.userinfo.dao.oracle;
+
+import ch13.domain.userinfo.UserInfo;
+import ch13.domain.userinfo.dao.UserInfoDao;
+
+public class UserInfoOracleDao implements UserInfoDao {
+    @Override
+    public void insertUserInfo(UserInfo userInfo) {
+        System.out.println("userInfo into ORACLE DB userID == " + userInfo.getUserId());
+    }
+
+    @Override
+    public void updateUSerInfo(UserInfo userInfo) {
+        System.out.println("userInfo into ORACLE DB userID == " + userInfo.getUserId());
+    }
+
+    @Override
+    public void deleteUserInfo(UserInfo userInfo) {
+        System.out.println("userInfo into ORACLE DB userID == " + userInfo.getUserId());
+    }
+}
