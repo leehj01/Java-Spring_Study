d5ab4cd73ffe528e2e26af591b2292a4acf8905c
leehjung55 <leehjung55@gmail.com>
2021-04-29 08:32:00 +0900

    ch 15

---
+++ Chapter3/src/ch15/MyClass.java
@@ -0,0 +1,18 @@
+package ch15;
+
+public class MyClass implements MyInterface{
+    @Override
+    public void myMethod() {
+        System.out.println("MyClass.myMethod");
+    }
+
+    @Override
+    public void x() {
+        System.out.println("MyClass.x");
+    }
+
+    @Override
+    public void y() {
+        System.out.println("MyClass.y");
+    }
+}
