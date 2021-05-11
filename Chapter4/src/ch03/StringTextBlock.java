3828a2f2d616b730bf409e77a97fb2224eb6f878
leehjung55 <leehjung55@gmail.com>
2021-05-11 13:16:09 +0900

    ch 04

---
+++ Chapter4/src/ch03/StringTextBlock.java
@@ -0,0 +1,26 @@
+package ch03;
+// java 13 에서 쓰임
+//public class StringTextBlock {
+//
+//    public static void main(String[] args) {
+//
+//        String strBlock = """
+//                This
+//                is
+//                text
+//                block
+//                text.""";
+//        System.out.println(strBlock);
+//
+//        System.out.println(getBlockOfHtml());
+//    }
+//
+//    public static String getBlockOfHtml(){
+//        return """
+//            <html>
+//                <body>
+//                    <span> example text </span>
+//                </body>
+//            </html>""";
+//    }
+//}
